009524e91d8266932dc1a4a67da704da7cb209b5
codeOfRobin <me@rmalhotra.com>
2016-12-24 20:39:42 +0530

    init

---
+++ ShadowKit.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+enum ShadowInput {
+	case view(view: UIView)
+	case image(image: UIImage)
+}
+
+
+func shadowMaker(input: ShadowInput) -> UIImage {
+	
+}
