3bad00b384b799d9cfb24e15ab5a617b15114c28
Mayur <jhonmathews@gmail.com>
2017-04-01 17:58:38 +0530

    Added comments to the Playground file.

--- ShadowKit.playground/Contents.swift
+++ ShadowKit.playground/Contents.swift
@@ -10,24 +10,31 @@ let zankyoReference = #imageLiteral(resourceName: "cover.jpg")
 let downtownFiction = #imageLiteral(resourceName: "ib524565.jpg")
 
 func applyBlurEffect(image: UIImage) -> UIImage {
-	let context = CIContext(options: nil)
-	let imageToBlur = CIImage(image: image)
-	let blurfilter = CIFilter(name: "CIGaussianBlur")
-	blurfilter!.setValue(imageToBlur, forKey: "inputImage")
-	let blurRadius = 90.0
-	let inset = CGFloat(blurRadius * 4.0 + 0.0)
-	blurfilter!.setValue(blurRadius, forKey: "inputRadius")
-	let resultImage = blurfilter!.value(forKey: "outputImage") as! CIImage
-	
-	let origExtent = imageToBlur!.extent
-	var newExtent = resultImage.extent
-	newExtent.origin.x += (newExtent.size.width - origExtent.size.width - inset * 0.75)/2
-	newExtent.origin.y += (newExtent.size.height - origExtent.size.height - inset * 0.75)/2
-	newExtent.size = CGSize(width: origExtent.size.width + inset * 0.75, height: origExtent.size.height + inset * 0.75)
-	
-	let cgImage = context.createCGImage(resultImage, from: newExtent)
-	let blurredImage = UIImage(cgImage: cgImage!)
-	return blurredImage
+    let context = CIContext(options: nil)
+    let imageToBlur = CIImage(image: image)
+    let blurfilter = CIFilter(name: "CIGaussianBlur")
+    blurfilter!.setValue(imageToBlur, forKey: "inputImage")
+    let blurRadius = 90.0
+    let inset = CGFloat(blurRadius * 4.0 + 0.0)
+    blurfilter!.setValue(blurRadius, forKey: "inputRadius")
+    let resultImage = blurfilter!.value(forKey: "outputImage") as! CIImage
+    
+    //What we hope to achieve at the end point it to return an image with a blur. This image should be slightly larger across bounds of the imageview, which will them super-impose the image on top of the blur, giving it the apple music album art effect
+    
+    //Figure out the extents of both the original and the blurred images:
+    let origExtent = imageToBlur!.extent
+    var newExtent = resultImage.extent
+    
+    //Normalise the bounds and center of the new extent to fit our requirement of 'slightly larger bounds than the original image'
+    //First, we re-orient the center:
+    newExtent.origin.x += (newExtent.size.width - origExtent.size.width - inset * 0.75)/2
+    newExtent.origin.y += (newExtent.size.height - origExtent.size.height - inset * 0.75)/2
+    //...and then re-size to fit:
+    newExtent.size = CGSize(width: origExtent.size.width + inset * 0.75, height: origExtent.size.height + inset * 0.75)
+    
+    let cgImage = context.createCGImage(resultImage, from: newExtent)
+    let blurredImage = UIImage(cgImage: cgImage!)
+    return blurredImage
 }
 
 class TestViewController: UIViewController {
